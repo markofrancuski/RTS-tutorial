3e1ccd1c26f2fa883a8507bcb83a2d0932c4665f
Darjan Rac <darjan.rac@marbleit.rs>
2022-07-08 16:04:48 +0200

    Start on Grid System.

---
+++ Assets/Scripts/Testing.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+
+    private GridSystem _gridSystem;
+    void Start()
+    {
+        _gridSystem = new GridSystem(10, 10, 2f);
+    }
+
+
+    private void Update()
+    {
+        Debug.Log(_gridSystem.GetGridPosition(MouseWorld.GetPosition()));
+    }
+}
